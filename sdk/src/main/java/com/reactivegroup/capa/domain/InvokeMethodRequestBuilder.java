b47633c3a9e6960dbeb6e929a75334b423b0a6f1
Kevin_T <596823919@qq.com>
2021-06-10 22:27:15 +0800

    chore: copy some class from dapr

---
+++ sdk/src/main/java/com/reactivegroup/capa/domain/InvokeMethodRequestBuilder.java
@@ -0,0 +1,52 @@
+package com.reactivegroup.capa.domain;
+
+/**
+ * Builds a request to invoke a service.
+ */
+public class InvokeMethodRequestBuilder {
+
+    private final String appId;
+
+    private final String method;
+
+    private String contentType;
+
+    private Object body;
+
+    private HttpExtension httpExtension = HttpExtension.NONE;
+
+    public InvokeMethodRequestBuilder(String appId, String method) {
+        this.appId = appId;
+        this.method = method;
+    }
+
+    public InvokeMethodRequestBuilder withContentType(String contentType) {
+        this.contentType = contentType;
+        return this;
+    }
+
+    public InvokeMethodRequestBuilder withBody(Object body) {
+        this.body = body;
+        return this;
+    }
+
+    public InvokeMethodRequestBuilder withHttpExtension(HttpExtension httpExtension) {
+        this.httpExtension = httpExtension;
+        return this;
+    }
+
+    /**
+     * Builds a request object.
+     * @return Request object.
+     */
+    public InvokeMethodRequest build() {
+        InvokeMethodRequest request = new InvokeMethodRequest();
+        request.setAppId(this.appId);
+        request.setContentType(contentType);
+        request.setMethod(this.method);
+        request.setBody(this.body);
+        request.setHttpExtension(this.httpExtension);
+        return request;
+    }
+
+}
