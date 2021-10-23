8f92b33e6923e13fc098cb2b9fea70e1bcc13d2b
Xiong, Pin <pinxiongcn@foxmail.com>
2021-10-23 13:27:26 +0800

    fix: Optimize codestyle check (#22)
    1. Check all unapproval files in unit test without Apache License
    2. Add Apache License in unit tests
    3. Update workflows to support codestyle check
    
    see issue: https://github.com/reactivegroup/capa/issues/21

--- sdk-spi/src/test/java/group/rxcloud/capa/spi/http/CapaSerializeHttpSpiTest.java
+++ sdk-spi/src/test/java/group/rxcloud/capa/spi/http/CapaSerializeHttpSpiTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package group.rxcloud.capa.spi.http;
 
 import group.rxcloud.capa.component.http.CapaHttp;
@@ -8,7 +24,13 @@ import group.rxcloud.capa.infrastructure.serializer.DefaultObjectSerializer;
 import group.rxcloud.capa.infrastructure.serializer.ObjectSerializer;
 import group.rxcloud.capa.spi.config.RpcServiceOptions;
 import group.rxcloud.cloudruntimes.utils.TypeRef;
-import okhttp3.*;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okhttp3.MediaType;
+import okhttp3.Protocol;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
