8c838e111a64d9b0e63d6368472e22e4629d9728
kevinten10 <596823919@qq.com>
2021-11-07 22:07:34 +0800

    feat: optimize pubsub subscriber aop

--- sdk-springboot/src/main/java/group/rxcloud/capa/springboot/pubsub/PubSubBeanPostProcessor.java
+++ sdk-springboot/src/main/java/group/rxcloud/capa/springboot/pubsub/PubSubBeanPostProcessor.java
@@ -6,29 +6,42 @@
 package group.rxcloud.capa.springboot.pubsub;
 
 import com.kevinten.vrml.core.beans.SpringContextConfigurator;
+import com.kevinten.vrml.core.serialization.Serialization;
+import group.rxcloud.capa.infrastructure.serializer.DefaultObjectSerializer;
+import group.rxcloud.capa.infrastructure.serializer.ObjectSerializer;
 import group.rxcloud.capa.pubsub.Topic;
-import group.rxcloud.capa.pubsub.domain.TopicSubscription;
 import group.rxcloud.capa.pubsub.domain.TopicEventRequest;
+import group.rxcloud.capa.pubsub.domain.TopicSubscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.beans.factory.config.EmbeddedValueResolver;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import reactor.core.publisher.Flux;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Map;
 
 /**
- * Handles Dapr annotations in Springboot Controllers.
+ * Handles Capa annotations in Springboot Controllers.
  */
 @Component
 public class PubSubBeanPostProcessor implements BeanPostProcessor {
 
+    private static final Logger logger = LoggerFactory.getLogger(PubSubBeanPostProcessor.class);
+
     private final EmbeddedValueResolver embeddedValueResolver;
+    private final ObjectSerializer serializer;
 
     PubSubBeanPostProcessor(ConfigurableBeanFactory beanFactory) {
         embeddedValueResolver = new EmbeddedValueResolver(beanFactory);
+        serializer = new DefaultObjectSerializer();
     }
 
     /**
@@ -40,7 +53,7 @@ public class PubSubBeanPostProcessor implements BeanPostProcessor {
             return null;
         }
 
-        subscribeToTopics(bean.getClass(), embeddedValueResolver);
+        subscribeToTopics(bean.getClass(), embeddedValueResolver, serializer);
 
         return bean;
     }
@@ -56,14 +69,15 @@ public class PubSubBeanPostProcessor implements BeanPostProcessor {
     /**
      * Subscribe to topics based on {@link Topic} annotations on the given class and any of ancestor classes.
      *
-     * @param clazz Controller class where {@link Topic} is expected.
+     * @param clazz      Controller class where {@link Topic} is expected.
+     * @param serializer json serializer
      */
-    private static void subscribeToTopics(Class clazz, EmbeddedValueResolver embeddedValueResolver) {
+    private static void subscribeToTopics(Class clazz, EmbeddedValueResolver embeddedValueResolver, ObjectSerializer serializer) {
         if (clazz == null) {
             return;
         }
 
-        subscribeToTopics(clazz.getSuperclass(), embeddedValueResolver);
+        subscribeToTopics(clazz.getSuperclass(), embeddedValueResolver, serializer);
 
         for (Method method : clazz.getDeclaredMethods()) {
             Topic topic = method.getAnnotation(Topic.class);
@@ -74,30 +88,71 @@ public class PubSubBeanPostProcessor implements BeanPostProcessor {
             // get TopicSubscriber instance from spring-context
             final TopicSubscriber topicSubscriber = SpringContextConfigurator.getBean(TopicSubscriber.class);
 
-            final String route = topic.name();
-
             final String topicName = embeddedValueResolver.resolveStringValue(topic.name());
             final String pubSubName = embeddedValueResolver.resolveStringValue(topic.pubsubName());
-            if ((topicName != null) && (topicName.length() > 0) && pubSubName != null && pubSubName.length() > 0) {
-                TopicSubscription topicSubscription = new TopicSubscription();
-                topicSubscription.setPubSubName(pubSubName);
-                topicSubscription.setTopicName(topicName);
+            boolean isValidTopic = (topicName != null) && (topicName.length() > 0) && pubSubName != null && pubSubName.length() > 0;
+            if (!isValidTopic) {
+                if (logger.isWarnEnabled()) {
+                    logger.warn("[PubSub.@Topic.subscribe] illegal pubsub[{}] topic[{}]",
+                            pubSubName, topicName);
+                }
+                continue;
+            }
+            final Map<String, String> metadata = resolveMetadataMap(serializer, topic.metadata());
 
-                Flux<TopicEventRequest> listener = topicSubscriber.doSubscribe()
-                        .apply(topicSubscription);
+            TopicSubscription topicSubscription = new TopicSubscription();
+            topicSubscription.setPubSubName(pubSubName);
+            topicSubscription.setTopicName(topicName);
+            topicSubscription.setMetadata(metadata);
 
-                listener.subscribe(topicEventRequest -> {
-                    try {
-                        method.invoke(topicEventRequest);
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException(e);
-                    } catch (InvocationTargetException e) {
-                        throw new RuntimeException(e);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
+            if (logger.isInfoEnabled()) {
+                logger.info("[PubSub.@Topic.subscribe] try to generate pubsub[{}] topic[{}] metadata[{}] listener",
+                        pubSubName, topicName, metadata);
+            }
+            // subscribe topic and generate the flux
+            Flux<TopicEventRequest> listener = topicSubscriber.doSubscribe()
+                    .apply(topicSubscription);
+
+            listener.subscribe(topicEventRequest -> {
+                try {
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("[PubSub.@Topic.subscribe] request[{}]", Serialization.toJsonSafe(topicEventRequest));
                     }
-                });
+                    method.invoke(topicEventRequest);
+                } catch (IllegalAccessException e) {
+                    if (logger.isWarnEnabled()) {
+                        logger.warn("[PubSub.@Topic.subscribe] IllegalAccessException, request[{}]", Serialization.toJsonSafe(topicEventRequest), e);
+                    }
+                    throw new RuntimeException(e);
+                } catch (InvocationTargetException e) {
+                    if (logger.isWarnEnabled()) {
+                        logger.warn("[PubSub.@Topic.subscribe] InvocationTargetException, request[{}]", Serialization.toJsonSafe(topicEventRequest), e);
+                    }
+                    throw new RuntimeException(e);
+                } catch (Exception e) {
+                    if (logger.isWarnEnabled()) {
+                        logger.warn("[PubSub.@Topic.subscribe] Exception, request[{}]", Serialization.toJsonSafe(topicEventRequest), e);
+                    }
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+    }
+
+    private static Map<String, String> resolveMetadataMap(ObjectSerializer serializer, String metadata) {
+        Map<String, String> metadataMap = Collections.emptyMap();
+        if (!StringUtils.isEmpty(metadata)) {
+            byte[] metadataBytes = metadata.getBytes(StandardCharsets.UTF_8);
+            try {
+                metadataMap = serializer.deserialize(metadataBytes, Map.class);
+            } catch (Exception e) {
+                if (logger.isErrorEnabled()) {
+                    logger.error("[PubSub.@Topic.subscribe] illegal metadata[{}]",
+                            metadata, e);
+                }
+                throw new RuntimeException(e);
             }
         }
+        return metadataMap;
     }
 }
--- sdk-springboot/src/main/java/group/rxcloud/capa/springboot/pubsub/TopicSubscriber.java
+++ sdk-springboot/src/main/java/group/rxcloud/capa/springboot/pubsub/TopicSubscriber.java
@@ -1,12 +1,20 @@
 package group.rxcloud.capa.springboot.pubsub;
 
-import group.rxcloud.capa.pubsub.domain.TopicSubscription;
 import group.rxcloud.capa.pubsub.domain.TopicEventRequest;
+import group.rxcloud.capa.pubsub.domain.TopicSubscription;
 import reactor.core.publisher.Flux;
 
 import java.util.function.Function;
 
+/**
+ * Defined customer topic subscriber implement.
+ */
 public interface TopicSubscriber {
 
+    /**
+     * Subscribe {@link TopicSubscription} and generate the message Flux.
+     *
+     * @return the high level function
+     */
     Function<TopicSubscription, Flux<TopicEventRequest>> doSubscribe();
 }
